125bbaf335025ce02c5651c8fb0f63a493f558fa
Ivan Karmanov <ivan@karmanow.ru>
2025-09-13 13:29:50 +0700

    fix nginx proxy prefix param of config

--- src/testovoe/main/main.py
+++ src/testovoe/main/main.py
@@ -6,7 +6,7 @@ from .dependencies import get_config
 
 def create_app():
     cfg = get_config()
-    if cfg.nginx_root_path is not None and cfg.nginx_root_path != "":
+    if cfg.nginx_proxy_prefix is not None and cfg.nginx_proxy_prefix != "":
         app = FastAPI(root_path="/api")
     else:
         app = FastAPI()
